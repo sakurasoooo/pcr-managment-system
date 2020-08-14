a7d58a6a85d116505030800e21deb27134ef38e2
Ying <liyi4@oregonstate.edu>
2020-08-13 20:31:33 -0700

    flip format

--- myapp/src/services/EventService.js
+++ myapp/src/services/EventService.js
@@ -1,55 +1,57 @@
 import axios from "axios"
 
+const url = "http://flip1.engr.oregonstate.edu:2333"
+
 export default {
   async getEvents() {
-    let res = await axios.get("http://localhost:2333/events");
+    let res = await axios.get(url+"/events");
     //alert(JSON.stringify(res.data));
     return res.data;
   },
   async getEventSingle(eventId) {
-    let res = await axios.get("http://localhost:2333/events/" + eventId);
+    let res = await axios.get(url+"/events/" + eventId);
     return res.data;
   },
 
   async sendEvent(item) {
-    return await axios.post('http://localhost:2333/save', item);
+    return await axios.post(url+'/save', item);
   },
 
   async _signup(item) {
-    return await axios.post('http://localhost:2333/signup', item);
+    return await axios.post(url+'/signup', item);
   },
   async _updateUser(item) {
-    return await axios.post('http://localhost:2333/update/user', item);
+    return await axios.post(url+'update/user', item);
   },
   async _login(username) {
-    let res = await axios.get("http://localhost:2333/login/"+username);
+    let res = await axios.get(url+"/login/"+username);
     return res.data;
   },
   async _getallclans() {
-    let res = await axios.get("http://localhost:2333/clanlist");
+    let res = await axios.get(url+"/clanlist");
     return res.data;
   },
   async _newclan(item) {
-    return await axios.post('http://localhost:2333/clan/newclan', item);
+    return await axios.post(url+'clan/newclan', item);
   },
 
   async _removeclan(item) {
-    return await axios.post('http://localhost:2333/clan/removeclan', item);
+    return await axios.post(url+'clan/removeclan', item);
   },
   async _isinclan(item){
-     let res = await axios.post('http://localhost:2333/clan/isinclan', item);
+     let res = await axios.post(url+'/clan/isinclan', item);
      return res.data
   },
 
   async _joinclan(item) {
-    return await axios.post('http://localhost:2333/clan/joinclan', item);
+    return await axios.post(url+'/clan/joinclan', item);
   },
   async _quitclan(item) {
-    return await axios.post('http://localhost:2333/clan/quit', item);
+    return await axios.post(url+'/clan/quit', item);
   },
 
   async _getclanmembers(item) {
-    let res = await axios.post('http://localhost:2333/clan/clanmembers', item);
+    let res = await axios.post(url+'/clan/clanmembers', item);
     return res.data
   },
 
--- server/dbcon.js
+++ server/dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_liyi4',
-  password        : '5962',
-  database        : 'cs340_liyi4'
+  user            : 'cs340_name',
+  password        : '0000',
+  database        : 'cs340_name'
 });
 module.exports.pool = pool;
